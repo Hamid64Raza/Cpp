c3e0679093a1238eb42277f24715e13910d5291d
Hamid Raza <112300409+Hamid64Raza@users.noreply.github.com>
2022-09-07 21:43:39 +0530

    Add files via upload

---
+++ m.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+int n,i=1;
+cout<<"Enter a number";
+cin>>n;
+do{
+cout<<i;
+i++;
+}
+while(i<n);
+{
+    cout<<i;
+    
+}
+
+return 0;
+}
