10ff2209dcc98d6e14be0b21155999f98e78b30a
Hamid Raza <112300409+Hamid64Raza@users.noreply.github.com>
2022-09-07 21:43:13 +0530

    Add files via upload

---
+++ l.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+ int n,i=1;
+ cout<<"Enter any to print upto that number";
+ cin>>n;
+ while(i<=n)
+ {
+    cout<<i;
+    i++;
+    
+ }
+return 0;
+}
