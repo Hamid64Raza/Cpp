7a4d8a4aa9be07a286c61ca4b1b0ce81c375e562
Hamid Raza <112300409+Hamid64Raza@users.noreply.github.com>
2022-09-17 22:46:19 +0530

    Add files via upload

---
+++ cb8.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class op
+{
+private:
+    int a,b,c;
+public:
+    void read();
+    void cal();
+    void print();
+};
+void op ::read()
+{
+    cout<<"Enter two no"<<endl;
+    cin>>a>>b;
+}
+void op::cal()
+{
+    c=a+b;
+}
+void op:: print()
+{
+    cout<<"SUM is ="<<c;
+}
+int main()
+{
+    op a1;
+    a1.read();
+    a1.cal();
+    a1.print();
+    return 0;
+}
