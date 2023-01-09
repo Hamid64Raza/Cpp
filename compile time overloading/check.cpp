9e268f2f7b4da93af46c5dd4b9a0336064a07ccb
Hamid Raza <112300409+Hamid64Raza@users.noreply.github.com>
2023-01-09 13:27:43 +0530

    Update check.cpp

--- compile time overloading/check.cpp
+++ compile time overloading/check.cpp
@@ -1,3 +1,5 @@
 Types of polymorphish are:-
   1>Runtime polymorphish
   2>Compile time Polymorphism
+  //Runtime polymorphish is implimented using Function and Operator Overloading.
+  //Compile time Polymorphish is Implemented using Virtual Function.
